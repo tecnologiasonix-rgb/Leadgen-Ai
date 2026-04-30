57fdf55e47823bbd51c4cf026fd4780b803f11df
tecnologiasonix-rgb <tecnologiasonix@gmail.com>
2026-04-30 23:28:40 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+console.log('[Diagnostic] Frontend loaded');
+// @ts-ignore
+console.log('[Diagnostic] GEMINI_API_KEY in browser:', !!process.env?.GEMINI_API_KEY);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
